515f62ca68a7a8fb7e698fdb29ce21c2973d822d
MauriDiPietro <dipietro.jm@gmail.com>
2025-08-15 10:22:52 -0300

    feat: cors

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
