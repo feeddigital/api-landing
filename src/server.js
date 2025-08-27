722262801b4f7aefdabf0312ea32a6044cde1b45
MauriDiPietro <dipietro.jm@gmail.com>
2025-08-27 13:12:15 -0300

    feat: envio mail clase intro

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import "dotenv/config";
 const app = express();
 
 app.use(express.json());
+app.use(express.static("public"));
 app.use(
   cors({
     origin: [process.env.FRONT_DEV, process.env.FRONT_PROD],
